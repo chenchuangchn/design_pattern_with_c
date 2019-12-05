97059dc0cfbaae6ff2dccd58edc9e139cd23b92c
chenchuang <chenchuangchn@gmail.com>
2019-12-06 00:41:30 +0800

    add lazy singleton pattern

---
+++ singleton_pattern/lazy_singleton.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <pthread.h>
+
+#include "singleton.h"
+
+
+static pthread_mutex_t singleton_mutex;
+
+static void singleton_initialize(struct singleton *psg, int x, int y)
+{
+    psg->priv_x = x;
+    psg->priv_y = y;
+}
+
+struct singleton *get_instance()
+{
+    static struct singleton *instance = NULL;
+
+    if (NULL == instance) {
+        pthread_mutex_lock(&singleton_mutex);
+        if (NULL == instance) {
+            printf("malloc instance\n");
+            instance = malloc(sizeof(struct singleton));
+            assert(NULL != instance);
+            singleton_initialize(instance, 1, 2);
+        }
+        pthread_mutex_unlock(&singleton_mutex);
+    }
+    return instance;
+}
